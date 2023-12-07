da6834d1eb8ed35de9571c5a672ea3d37bd255c5
Ouassim Chakir <oua.chakir@edu.umi.ac.ma>
2023-12-07 19:42:26 +0100

    master

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,23 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/task.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TasksModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: 'mysql',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'wassim2001',
+      port: 3306,
+      username: 'root',
+      password: '',
       database: 'task-management',
       autoLoadEntities: true,
       entities: [Task],
       synchronize: true,
     }),
+    AuthModule,
   ],
 })
 export class AppModule {}
